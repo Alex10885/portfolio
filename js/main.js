4964eba96d23a08e9c1203f0aff92591975c973b
Alexey Doronin <aledor10885@gmail.com>
2020-01-24 20:17:15 +0300

    готовая верстка

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.projects__pagination',
+        bulletClass: 'projects__bullet',
+        bulletActiveClass: 'projects__bullet_active',
+        clickable: 'true'
+    }
+});

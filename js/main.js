49d9289b4c0b2c648e052fd87b358fb7d94be7a7
Alexey Doronin <aledor10885@gmail.com>
2020-01-30 07:01:29 +0300

    модальное окно консультации

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ var mySwiper = new Swiper('.swiper-container', {
         clickable: 'true'
     }
 });
+
+$(document).ready(function () {
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks').fadeOut('slow');
+    });
+
+
+});

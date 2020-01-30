6eb949fa42b153c7c1cc42c4227a4c22334a1832
Alexey Doronin <aledor10885@gmail.com>
2020-01-30 19:00:02 +0300

    настроен скролл вверх

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ $(document).ready(function () {
     $("a[href^='#']").click(function () {
         const _href = $(this).attr("href");
         $("html, body").animate({
-            scrollTop: $(_href).offset().top + "px"
+            scrollTop: $(_href).offset().top - 15 + "px"
         });
         return false;
     });
